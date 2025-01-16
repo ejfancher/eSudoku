c893f5e3393d58a7cd1d0de9e72f7d1c0f59ec35
ejfancher <evanjfancher@gmail.com>
2025-01-16 06:58:53 -1000

    fuck @ben

--- server.js
+++ server.js
@@ -2,42 +2,49 @@ const express = require('express');
 const fs = require('fs');
 const fsP = require('fs').promises;
 const boards = require('./boards.js');
-
-const app = express();
+const http = require('http');
 
 const cwd = __dirname
-/*
 
-function requestCallback(request/* an http.IncomingMessage as per bullets below the event (also HttpServer has no <ImcomingMeesage> property) , response) {
+//const app = express()
+
+function requestCallback(request/* an http.IncomingMessage as per bullets below the event (also HttpServer has no <ImcomingMeesage> property) */, response) {
     let httpServer = this; // (assuming this function we're in is a "ordinary listener function") based on event module
     console.log(this);
+    console.log('request url: '+request.url)
     if (request.url == "/") {
-        let target_file = cwd+'/src/html/home_page.html';
+        let target_file = cwd+'/src/html/site_map.html';
         response.write(fs.readFileSync(target_file));
         response.end();
     }
-    else if (request.url === '/game' || request.url.startsWith('') {
-        let randBoardPromise = new Promise( (R, r) => { r(boards.randomBoard(diff)) });
-        let filePromise = fsP.readFile('./src/html/game_page.html', 'utf8').then
-        Promise.all(randBoardPromise, filePromise).then(
+    else if (request.url === '/game' ){
+        var diff = 'easy';
+        let randBoardPromise = new Promise( (R, r) => { R(boards.randomBoard(diff))} );
+        let filePromise = fsP.readFile('./src/html/game_page.html', 'utf8');
+        Promise.all(randBoardPromise, filePromise).then(function(results){
+                var data=results[1];
+                var split_point = data.indexOf('<script src="./bundle.js">');
+                var before = data.slice(0, split_point);
+                var after = data.slice(split_point);
+                data = before+'<script type="text/javascript">var board_string=\''+results[1]+'\'</script>\n'+after;
+                
+                response.write(data);
+        });
+        response.end();
     }
 }
-*/
+
 
 // hosting static files
-var static_files_root_dir = cwd + '/src/css'
-app.use(express.static(static_files_root_dir))
+//var static_files_root_dir = cwd + '/src/css'
+//app.use(express.static(static_files_root_dir))
 
-app.get('/', (req, res) => {
-	var target_file = cwd+'/src/html/home_page.html'
-	res.sendFile(target_file)
-})
-
+/* 
 app.get('/game', (req, res) => {
   const diff = req.query.diff
-/* if a difficulty option came through in the query portion of the
+if a difficulty option came through in the query portion of the
 requested resource, inject a random board of that difficulty into
-the game page and serve that page */
+the game page and serve that page
   if (diff != null && diff != "") {
     const board_string = boards.randomBoard(diff)
     var data = fs.readFileSync('./src/html/game_page.html', 'utf8')
@@ -52,6 +59,7 @@ the game page and serve that page */
   }
 })
 
+
 app.get('/bundle.js', (req, res) => {
   var target_file = cwd+'/src/bundle/bundle.js'
   res.sendFile(target_file)
@@ -61,7 +69,7 @@ app.get('/map', (req, res) => {
   var target_file = cwd+'/src/html/site_map.html'
   res.sendFile(target_file)
 })
-
+*/
 // check for port env. variable to use when being ran on heroku server
 // otherwise run on port 5000 locally
 let port = process.env.PORT;
@@ -69,4 +77,5 @@ if (port == null || port == "") {
   port = 5000;
 }
 
-app.listen(port);
+//app.listen(port);
+http.createServer(requestCallback).listen(port);
